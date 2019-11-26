0fee5d45e0ba10ac941691cb74bfbfd9852fe80b
PolskiSlimak <slimak-75@wp.pl>
2019-11-26 21:53:08 +0100

    aktualizacja

--- Player.java
+++ Player.java
@@ -1,41 +1,31 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javaapplication7;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/**
- *
- * @author Student
- */
 public abstract class Player { //musi mieć conajmniej jedna metode abstrakcyjna
 
     private String name;
-    public Player() {
-    }
 
-    public Player(String name) {
+    public Player(){}
+
+    public Player(String name){
         setName(name);
     }
 
-    public String getName() {
+    public final String getName() { //jeżeli dajemy final do settera to do gettera też
         return name;
     }
 
-    public final void setName(String name) {
-        if (name != null && name.matches("^[a-zA-Z~]{3,}$")) // bezpieczniejszy sposob, bo name moze nie istniec, a pusty lancuch znakow zawsze istnieje
-        {
+    public final void setName(String name) { //final - ostateczna wersja metody, brak możliwości przesłonięcia
+        if(name!=null /*&&  name.matches("^[a-zA-Z~]{3,}$")*/) // bezpieczniejszy sposob, bo name moze nie istniec, a pusty lancuch znakow zawsze istnieje
             this.name = name; //this wskazuje na obiekt n którym jest wywoływana metoda setName
-        } else //            System.err.println("Name can't be null");
-        {
-            throw new IllegalArgumentException("Dales nulla");
+        else{
+            //System.err.println("Name can't be null");
+            throw new IllegalArgumentException("Nieprawidłowe imię!"); //rzucamy wyjątek, przechodzi z playera do maina
         }
+
     }
 
     public abstract int guess();
-}
 
+    @Override
+    public String toString(){
+        return name;
+    }
+}
