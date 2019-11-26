951ba1d5ebaec7051e2bc010b43d1b0920a19a91
PolskiSlimak <slimak-75@wp.pl>
2019-11-26 21:52:21 +0100

    aktualizacja

--- Main.java
+++ Main.java
@@ -1,56 +1,65 @@
-﻿package javaapplication7;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
-public class JavaApplication7 {
+public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       /* Random random = new Random();
-        Player player = new PlayerHuman();
-        List<String> list = new ArrayList();
-        list.add("jeden");
-        list.add("dwa");
-        list.add("trzy");
-        //String lista =list.toString().replace(",", "").replace("[", "").replace("]", "").trim();
-        //System.out.println(lista);
-        /*for (int i = 0; i < list.size(); ++i) {
-            System.out.println(list.get(i));
-        }
-        for (String s : list) {
-            System.out.println(s);
-        }*/
-        /*Iterator<String> it=list.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
-        }*/
-        for(Iterator<String> it=list.iterator();it.hasNext();){
-            System.out.println(it.next());
-        }
-        Game game = new Game();//playera można uogólnić, cząstką każdego innego playera będzie player
-        game.addPlayer(player);
-        game.play();
-//konstrukor wskazuje obiekt jakiej klasy chcemy utworzyc, polimorfizm - wielopostaciowosc, dany obiekt moze przyjmowac wiele postaci
-        /*try {
-            player.setName("bz13");
-        } catch (IllegalArgumentException ex) {
-            System.err.println("BLAD " + ex.getMessage());
-        }*/
-*/
+
         Random random=new Random();
-        Game game1=new Game();
-        game1.addPlayer(new PlayerHuman("Janusz"));
-        game1.addPlayer(new PlayerHuman("Janusz"));
-        game1.addPlayer(new PlayerHuman("Janusz"));
+         //playera można uogólnić, cząstką każdego innego playera będzie player
+        Statistics stats=new Statistics();
+        Game game1=new Game(null);
+
+//        List <String> list=new ArrayList();
+//        list.add(player.getName());
+
+//        for(int i=0; i<list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+
+//        Iterator itr=list.iterator();
+
+//        while(itr.hasNext()){
+//            System.out.println(itr.next());
+//        }
+//
+//        for(Iterator<String> itr2=list.iterator(); itr2.hasNext(); ){
+//            System.out.println(itr2.next());
+//        }
+
+
+//        try {
+//            player.setName(null); //regex101.com [a-zA-Z~]{3,}
+//        } catch (IllegalArgumentException ex) {
+//            System.err.println("BŁĄD!" + ex.getMessage()); //można dodawać wiele catchy, każdy dla innego wyjątku
+//        } catch (Exception ex){
+//            System.out.printf("Skontaktuj się z administratorem."); // złapie każdy inny wyjątek niż te wyżej
+//        }
+
+        //konstrukor wskazuje obiekt jakiej klasy chcemy utworzyc, polimorfizm - wielopostaciowosc, dany obiekt moze przyjmowac wiele postaci
+
+
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+//        game1.addPlayer(new PlayerComputer("Justyna"));
+
+        game1.addPlayer(new PlayerComputer("Justyna"));
+        game1.addPlayer(new PlayerComputer("Ania"));
+
         game1.play();
-        game1.printPlayers();
+        game1.play();
+        game1.play();
+
+        System.out.println();
+        game1.printStats();
+
+        //game1.showPlayers();
+
 
     }
-
 }
